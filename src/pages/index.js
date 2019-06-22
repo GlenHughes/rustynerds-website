33f1ef2a05d9a7085e50e34db9add3dbdbf4d794
Glen Hughes <glen.866@gmail.com>
2019-06-22 16:14:21 +0100

    Use image gatyby way of using images on slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,37 +4,27 @@ import "../../node_modules/react-image-gallery/styles/css/image-gallery.css"
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import slide1 from "../images/slider/01.png"
-import slide2 from "../images/slider/02.png"
-import slide3 from "../images/slider/03.png"
-import slide4 from "../images/slider/04.png"
-import slide5 from "../images/slider/05.png"
+import Image from "../components/image"
 
-const images = [
- {
-   original: slide1,
-   thumbnail: slide1,
- },
- {
-   original: slide2,
-   thumbnail: slide2,
- },
- {
-   original: slide3,
-   thumbnail: slide3,
- },
- {
-   original: slide4,
-   thumbnail: slide4,
- },
- {
-   original: slide5,
-   thumbnail: slide5,
- },
-]
-
-
-function IndexPage() {
+function IndexPage () {
+  const images = [
+    {
+      original: <Image filename="01.png" />,
+      thumbnail: <Image filename="01.png" />,
+    },
+    {
+      original: <Image filename="03.png" />,
+      thumbnail: <Image filename="03.png" />,
+    },
+    {
+      original: <Image filename="04.png" />,
+      thumbnail: <Image filename="04.png" />,
+    },
+    {
+      original: <Image filename="05.png" />,
+      thumbnail: <Image filename="05.png" />,
+    },
+  ]
   return (
     <Layout>
       <SEO
